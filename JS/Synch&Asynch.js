aa68ba134721f6fb2aacbeecb6a578034c9c19c7
manju1813 <manjunathaksmanju2004@gmail.com>
2026-07-30 09:43:52 +0530

    done

---
+++ JS/Synch&Asynch.js
@@ -0,0 +1,30 @@
+// Synchronous JS
+//todo -> Waiting for previous operations to complete it
+// console.log("Start")
+// alert("Suuuuiii")
+// console.log(2+3)
+// console.log("Hii")
+// console.log("END")
+
+// Asynchronous JS
+//todo -> The code will get execute without waitng for the previous task to complete it
+// console.log("Start")
+// setTimeout(() => {
+//     console.log("I will be executing after 3 Seconds")
+//     alert("Suuuuiii")
+// }, 3000);
+// console.log(2+3)ter
+// console.log("Hii")
+// console.log("END")
+
+// // setTimeout() method
+// setTimeout(() => {
+//     console.log("I will be executing after 3 Seconds")
+//     alert("Suuuuiii")
+// }, 3000);
+
+//setInterval() methoc
+// setInterval(() => {
+//     console.log("I will be executing after 3 Seconds")
+// }, 3000);
+
